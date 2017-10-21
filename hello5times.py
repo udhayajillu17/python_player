df9daa034d40cd821c3c90d291110658435a7a3d
udhayajillu17 <32951386+udhayajillu17@users.noreply.github.com>
2017-10-21 15:41:46 +0530

    Add files via upload

---
+++ hello5times.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse041
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse041 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    count = 5
+    while (count < 9):
+        print 'The count is:', count
+        count = count + 5
+print "hello!"
+
+
+if __name__ == '__main__':
+    main()
